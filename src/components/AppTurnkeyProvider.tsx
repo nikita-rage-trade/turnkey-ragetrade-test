ea226f5fa14b10fae210c000e7b138d3118cf8aa
nikita-rage-trade <none>
2025-02-13 00:48:57 +0300

    feat: added AppTurkeyProvider

---
+++ src/components/AppTurnkeyProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { TurnkeyProvider } from "@turnkey/sdk-react";
+import { ReactNode } from "react";
+
+interface AppTurnkeyProviderProps {
+  children: ReactNode;
+}
+
+const config = {
+  apiBaseUrl: "https://api.turnkey.com",
+  defaultOrganizationId: process.env.NEXT_PUBLIC_ORGANIZATION_ID as string
+};
+
+export default function AppTurnkeyProvider({
+  children
+}: AppTurnkeyProviderProps) {
+  return <TurnkeyProvider config={config}>{children}</TurnkeyProvider>;
+}
